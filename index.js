30c0cf1f79b8c41233700871d003c7ae2f577ec2
Alex Zdatny <alexzdatny@gmail.com>
2018-12-17 21:59:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function BoardMember(name, homeState, training){
-  this.name = name
-  this.homeState = homeState
-  this.training = training
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
   this.veto = function() {
-    console.log(`No, I must disagree`)
+    console.log("No, I must disagree");
   }
 }
